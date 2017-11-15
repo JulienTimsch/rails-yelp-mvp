a18db1cedb6ecf2ff45d0938e70ff42f0ab32999
Julien Schmit <julien.schmit@ehl.ch>
2017-11-15 12:37:42 +0100

    uploading models with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
